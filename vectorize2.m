fa66b72c0ad4bf7e7e31643f3f79a9277d8f90c9
William T <william.tam18@gmail.com>
2018-07-10 16:51:37 +0800

    add vec2

---
+++ vectorize2.m
@@ -0,0 +1,24 @@
+A = magic(10);
+x = ones(10,1);
+
+v = zeros(10, 1);
+for i = 1:10
+  for j = 1:10
+    v(i) = v(i) + A(i, j) * x(j);
+  end
+end
+
+disp('v')
+v
+
+% disp('1')
+% x*A
+
+% disp('2')
+% Ax
+
+disp('3')
+x' * A
+
+disp('4')
+sum(A * x)
